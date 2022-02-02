b1cd99dd8cc2f6f22efe2b5a58626b21682bd90f
Drew Daniels <drew@thedaniels.com>
2022-02-02 13:41:44 -0600

    Chaining the latitudes and longitudes obtained from the current weather data api to the call made to the one call API to get 7 day and hourly forecats for a city that can be entered by the user.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,19 @@ const searchBtn = document.querySelector('button');
 const displayElement = document.querySelector('p');
 
 const API_KEY = '9337f92135b1f0193cefc57b9b2c3d3b';
-const url = 'http://api.openweathermap.org/data/2.5/weather?';
+const CURRENT_WEATHER_DATA_URL = 'http://api.openweathermap.org/data/2.5/weather?';
+const ONE_CALL_API_URL = 'https://api.openweathermap.org/data/2.5/onecall?';
 
 function getCity() {
   return searchBox.value;
 }
 
-function getQryStr(city) {
+function getFilledCurrentWeatherQryStr(city) {
   return `q=${city}&units=Imperial&APPID=${API_KEY}`;
+}
+
+function getFilledOneCallAPIQryStr(lat, lon) {
+  return `lat=${lat}&lon=${lon}&exclude=minutely&appid=${API_KEY}`;
 }
 
 function getFullURL(baseURL, qryStr) {
@@ -21,10 +26,18 @@ searchBtn.addEventListener('click', main);
 
 async function main() {
   const city = getCity();
-  const qryStr = getQryStr(city);
-  const fullURL = getFullURL(url, qryStr);
-  const response = await fetch(fullURL);
-  const weatherData = await response.json();
-  console.log(weatherData);
-  displayElement.innerText = weatherData.main.temp;
+  const currentWeatherQryStr = getFilledCurrentWeatherQryStr(city);
+  const fullCurrentWeatherURL = getFullURL(CURRENT_WEATHER_DATA_URL, currentWeatherQryStr);
+  const currentWeatherResponse = await fetch(fullCurrentWeatherURL);
+  const currentWeatherData = await currentWeatherResponse.json();
+  console.log(currentWeatherData);
+  const lat = currentWeatherData.coord.lat;
+  const lon = currentWeatherData.coord.lon;
+  console.log(lat, lon);
+  const oneCallAPIQryStr = getFilledOneCallAPIQryStr(lat, lon);
+  const fullOneCallAPIURL = getFullURL(ONE_CALL_API_URL, oneCallAPIQryStr);
+  const oneCallAPIResponse = await fetch(fullOneCallAPIURL);
+  const oneCallAPIData = await oneCallAPIResponse.json();
+  console.log(oneCallAPIData.daily);
+  console.log(oneCallAPIData.hourly);
 }
