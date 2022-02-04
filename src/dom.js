850e054424d9378eba57c5462d60731f0f9edd8b
Drew Daniels <drew@thedaniels.com>
2022-02-04 09:16:48 -0600

    Created the banner and the searchbox.

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,6 @@
 import weatherAppIcon from './images/icons/weather-app-icon.svg';
 import githubIcon from './images/icons/github-icon.png';
+import searchIcon from './images/icons/search-icon.svg';
 // const searchBoxInput = document.querySelector('.searchbox-input');
 // const searchBoxBtn = document.querySelector('.searchbox-btn');
 // const cityText = document.querySelector('.city-text');
@@ -33,9 +34,112 @@ function getFavicon(faviconPath) {
   return faviconNode;
 }
 
+function getH1(h1Text, ...classes) {
+  const h1 = getDOMNode('h1', ...classes);
+  h1.innerText = h1Text;
+  return h1;
+}
+
+function getBanner() {
+  const banner = getDOMNode('div', 'banner');
+  const bannerImg = getImage(weatherAppIcon, 'Hurricane symbol', 'banner-img');
+  const bannerHeader = getH1('Weather App', 'banner-text');
+  banner.append(bannerImg, bannerHeader);
+  return banner;
+}
+
+function getInput(inputType, placeholderText, ...classes) {
+  const input = getDOMNode('input', ...classes);
+  input.type = inputType;
+  if (placeholderText) {
+    input.placeholder = placeholderText;
+  }
+  return input;
+}
+
+function getTextInput(placeholderText, ...classes) {
+  const input = getInput('text', placeholderText, ...classes);
+  return input;
+}
+
+function getButton(...btnClasses) {
+  const btn = getDOMNode('button', ...btnClasses);
+  return btn;
+}
+/**
+ * 
+ * @param {string} btnImgPath 
+ * @param {string} btnImgAlt 
+ * @param {array} btnImgClasses 
+ * @param  {...string} btnClasses 
+ * @returns button DOM node with a nested img
+ */
+function getImgButton(btnImgPath, btnImgAlt, btnImgClasses, ...btnClasses) {
+  const btn = getButton(...btnClasses);
+  const img = getImage(btnImgPath, btnImgAlt, ...btnImgClasses);
+  btn.appendChild(img);
+  return btn;
+}
+
+/**
+ * 
+ * @param {string} btnImgPath 
+ * @param {string} btnImgAlt 
+ * @param {array} btnImgClasses 
+ * @param {string} btnSpanText 
+ * @param {array} btnSpanClasses 
+ * @param  {...string} btnClasses 
+ * @returns button DOM node with nested img and span nodes
+ */
+function getImgAndSpanButton(
+  btnImgPath, 
+  btnImgAlt,
+  btnImgClasses,
+  btnSpanText,
+  btnSpanClasses, 
+  ...btnClasses) {
+  const btn = getImgButton(btnImgPath, btnImgAlt, ...btnImgClasses, ...btnClasses);
+  const btnSpan = getSpan(btnSpanText, ...btnSpanClasses);
+  btn.appendChild(btnSpan);
+  return btn;
+}
+
+function getSearchbox() {
+  const searchbox = getDOMNode('div', 'searchbox');
+  const searchboxInput = getTextInput('Enter your city', 'searchbox-input');
+  const searchboxButton = getImgButton(searchIcon, 'Magnifying Glass', 'searchbox-img', 'btn-base', 'searchbox-btn');
+  searchbox.append(searchboxInput, searchboxButton);
+  return searchbox;
+}
+
+function getUI() {
+  const ui = getDOMNode('div', 'ui');
+  const searchbox = getSearchbox();
+
+  ui.append(searchbox);
+  return ui;
+}
+
+function getHeader(...classes) {
+  const header = getDOMNode('header', ...classes);
+  const banner = getBanner();
+  const ui = getUI();
+
+  header.append(banner, ui);
+  return header;
+}
+
+function getMain(...classes) {
+  const main = getDOMNode('main', ...classes);
+
+  return main;
+}
+
 function getContentContainer() {
   const content = getDOMNode('div', 'content');
-
+  const header = getHeader('header');
+  const main = getMain('panels');
+  content.append(header, main);
   return content;
 }
 
@@ -54,15 +158,18 @@ function getSpan(spanText, ...classes) {
   return span;
 }
 
-function getImage(imagePath, ...classes) {
+function getImage(imagePath, alt, ...classes) {
   const img = getDOMNode('img', ...classes);
   img.src = imagePath;
+  if (alt) {
+    img.alt = alt;
+  }
   return img;
 }
 
 function getGithubAppAnchor() {
   const anchor = getAnchor('https://github.com/Drew-Daniels/weather_app');
-  const img = getImage(githubIcon, 'github-icon');
+  const img = getImage(githubIcon, 'Github Icon', 'github-icon');
   anchor.append(img);
   return anchor;
 }
@@ -88,15 +195,17 @@ function getFooter() {
 }
 
 function startup() {
+  // HEAD
   const head = document.querySelector('head');
   const favicon = getFavicon(weatherAppIcon);
   head.appendChild(favicon);
 
+  // BODY
   const body = document.querySelector('body');
   body.classList.add('content-container');
-
+  const content = getContentContainer();
   const footer = getFooter();
-  body.appendChild(footer);
+  body.append(content, footer);
 }
 
 export default {
